e5103194a877922c9988300769e5c105161b3235
Ryan Wilcox <rwilcox@wilcoxd.com>
2010-04-16 09:23:42 -0400

    create our form and validator for the newsletter form

--- turbogears2_sprox_tutorial/controllers/root.py
+++ turbogears2_sprox_tutorial/controllers/root.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Main Controller"""
 
-from tg import expose, flash, require, url, request, redirect
+from tg import expose, flash, require, url, request, redirect, tmpl_context, validate
 from pylons.i18n import ugettext as _, lazy_ugettext as l_
 
 from turbogears2_sprox_tutorial.lib.base import BaseController
@@ -10,12 +10,32 @@ from turbogears2_sprox_tutorial.controllers.error import ErrorController
 
 from turbogears2_sprox_tutorial import model
 
+from formencode import Schema
+from formencode.validators import FieldsMatch
+from tw.forms import TextField
+from sprox.formbase import AddRecordForm
+
+from turbogears2_sprox_tutorial.model.newsletter_subscriber import NewsletterSubscriber
+
 __all__ = ['RootController']
 
 from catwalk.tg2 import Catwalk
 
+newsletter_validator = Schema(chained_validators = (FieldsMatch("email_address", "verify_email_address", messages={"invalidNoMatch": "Email addresses do not match"}), ))
+# validator to make sure fields match. From the example at <http://sprox.org/index.html>
+
 class UnSecuredCatwalk(Catwalk):
     allow_only = None
+
+class NewsletterAddForm(AddRecordForm):
+    """A form that is an Add-A-Record form. Sprox has classes for all the CRUD operations"""
+    __model__ = NewsletterSubscriber                            # required
+    __omit_fields__ = ['id']                                    # skip display for the ID field (would be disabled anyway)
+    __require_fields = ["full_name", "email_address"]           # auto validation of these fields
+    verify_email_address = TextField("verify_email_address")    #add a non-model field, which our validator will compare
+    __base_validator__ = newsletter_validator                   # hook up our special validator to make sure two fields match
+
+new_newsletter_sub_form = NewsletterAddForm(DBSession)
 
 class RootController(BaseController):
     """
